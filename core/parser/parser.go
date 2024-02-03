80edaf1fe8fe4f24fa0a1ef28c8ea26f099f9718
Jhon-2801 <jangulovides@gmail.com>
2024-02-03 14:39:22 -0500

    Parser advances

--- core/parser/parser.go
+++ core/parser/parser.go
@@ -16,7 +16,10 @@ type Parser struct {
 
 // NewParser creates a new Parser instance with the provided lexer.
 func NewParser(lexer *lexer.Lexer) *Parser {
-	return &Parser{lexer: lexer}
+	parser := &Parser{lexer: lexer}
+	parser.nextToken()
+	parser.nextToken() // Call this twice to initialize current and peek.
+	return parser
 }
 
 // Return true if the current token matches.
@@ -34,14 +37,46 @@ func (p *Parser) match(Kind lexer.TokenInfo) {
 	if !p.checkPeek(Kind.TokenType) {
 		p.abort("Expected" + Kind.Name + ", got " + p.curToken.Kind.Name)
 	}
+	p.nextToken()
 }
 
 // Advances the current token
-func (p *Parser) nextToken(Kind lexer.TokenType) {
-
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lexer.GetToken()
+	//No need to worry about passing the EOF, lexer handles that.
 }
 
+//Production rules.
+
+// program ::= {statement}
+func (p *Parser) Program() {
+	fmt.Println("PROGRAM")
+	//Parse all the statements in the program.
+	for !p.checkToken(lexer.EOF) {
+
+	}
+}
 func (p *Parser) abort(message string) {
 	fmt.Printf("Error. %s\n", message)
 	os.Exit(1)
+}
+
+//One of the following statements
+
+func (p *Parser) statement() {
+	//Check the first token to see what kind of statement this is.
+
+	//"PRINT" (expression | string)
+	if p.checkToken(lexer.PRINT) {
+		print("STATEMENT-PRINT")
+		p.nextToken()
+
+		if p.checkToken(lexer.STRING) {
+			//Simple string
+			p.nextToken()
+		} else {
+
+		}
+	}
 }
